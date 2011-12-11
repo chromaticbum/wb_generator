c94e9f776b152ff1dfd92e8b55a5f4f149a662f9
Hollin Wilkins <chromaticbum@gmail.com>
2011-12-11 13:27:11 -0800

    Not storing matrix as a flat tuple anymore.

--- src/wb_board.erl
+++ src/wb_board.erl
@@ -5,9 +5,8 @@
 -export([
     create_board/1, create_board/2,
     random_letter/0,
-    position/2, position/3,
-    letter/2, letter/3,
-    rotate_position/4,
+    letter/3,
+    rotate_position/5,
     word_count/1
   ]).
 
@@ -15,74 +14,75 @@ create_board({Rows, Columns}) ->
   create_board(Rows, Columns).
 
 create_board(Rows, Columns) ->
-  Matrix = create_matrix(Rows, Columns),
+  Matrix = create_letter_matrix(Rows, Columns),
   #board{rows = Rows, columns = Columns, matrix = Matrix}.
 
-create_matrix(Rows, Columns) ->
+create_letter_matrix(Rows, Columns) ->
   {A1, A2, A3} = now(),
   random:seed(A1, A2, A3),
 
+  create_matrix(Rows, Columns, fun() -> random_letter() end).
+
+create_matrix(Rows, Columns, Generate) ->
   list_to_tuple(lists:map(
-    fun(_Index) -> random_letter() end, lists:seq(1, Rows * Columns)
+    fun(_Index) -> create_row(Columns, Generate) end,
+    lists:seq(1, Rows)
+  )).
+
+create_row(Columns, Generate) ->
+  list_to_tuple(lists:map(
+    fun(_Index) -> Generate() end,
+    lists:seq(1, Columns)
   )).
 
 create_used_matrix(Rows, Columns) ->
-  list_to_tuple(lists:map(
-    fun(_Index) -> false end, lists:seq(1, Rows * Columns)
-  )).
+  create_matrix(Rows, Columns, fun() -> false end).
 
-position(#board{columns = Columns}, Index) ->
-  {(Index - 1) div Columns + 1, ((Index - 1) rem Columns) + 1}.
+rotate_position(#board{rows = Rows, columns = Columns}, Row, Column, RotRow, RotColumn) ->
+  {((Row + RotRow - 1) rem Rows) + 1, ((Column + RotColumn - 1) rem Columns) + 1}.
 
-position(#board{columns = Columns}, Row, Column) ->
-  (Row - 1) * Columns + Column.
-
-rotate_position(#board{rows = Rows, columns = Columns} = Board, Index, RotRow, RotColumn) ->
-  {Row, Column} = wb_board:position(Board, Index),
-  {Row2, Column2} = {((Row + RotRow - 1) rem Rows) + 1, ((Column + RotColumn - 1) rem Columns) + 1},
-
-  position(Board, Row2, Column2).
-
-letter(#board{matrix = Matrix}, Index) ->
-  element(Index, Matrix).
-
-letter(Board, Row, Column) ->
-  letter(Board, position(Row, Column)).
+letter(#board{matrix = Matrix}, Row, Column) ->
+  element(Column, element(Row, Matrix)).
 
 word_count(#board{rows = Rows, columns = Columns} = Board) ->
-  Node = wb_tree:get_node(0),
+  RootNode = wb_tree:get_node(0),
   Used = create_used_matrix(Rows, Columns),
 
   lists:foldl(
-    fun(Index, WordCount) ->
-      WordCount + word_count(Board, position(Board, Index), Node, Used)
-    end, 0, lists:seq(1, Rows * Columns)
+    fun(Row, WordCount) ->
+      WordCount + lists:foldl(
+        fun(Column, WordCountRow) ->
+          WordCountRow + word_count(Board, Row, Column, RootNode, Used)
+        end, 0, lists:seq(1, Columns)
+      )
+    end, 0, lists:seq(1, Rows)
   ).
 
-word_count(Board, Position, Node, Used) ->
-  word_count(Board, Position, Node, Used, 1).
+word_count(Board, Row, Column, Node, Used) ->
+  word_count(Board, Row, Column, Node, Used, 1).
 
-word_count(#board{rows = Rows, columns = Columns} = Board, {Row, Column}, {_Id, _Terminal, Ids}, Used, Length) ->
-  Index = position(Board, Row, Column),
-  case (Row >= 1) andalso (Column >= 1) andalso (Row =< Rows) andalso (Column =< Columns) andalso (not element(Index, Used)) of
+word_count(#board{rows = Rows, columns = Columns} = Board, Row, Column, {_Id, _Terminal, Ids}, Used, Length) ->
+  case (Row >= 1) andalso (Column >= 1) andalso (Row =< Rows) andalso (Column =< Columns) andalso (not element(Column, element(Row, Used))) of
     true ->
-      Letter = letter(Board, Index),
+      Letter = letter(Board, Row, Column),
       Id = element(Letter - $a + 1, Ids),
       wb_tree:get_node(Id),
 
       case wb_tree:get_node(Id) of
         not_found -> 0;
         Node ->
-          Used2 = setelement(Index, Used, true),
+          UsedRow = setelement(Column, element(Row, Used), true),
+          Used2 = setelement(Row, Used, UsedRow),
+
           {_Id2, Terminal, _Ids2} = Node,
-          Count = word_count(Board, {Row + 1, Column}, Node, Used2, Length + 1)
-            + word_count(Board, {Row - 1, Column}, Node, Used2, Length + 1)
-            + word_count(Board, {Row, Column + 1}, Node, Used2, Length + 1)
-            + word_count(Board, {Row, Column - 1}, Node, Used2, Length + 1)
-            + word_count(Board, {Row + 1, Column + 1}, Node, Used2, Length + 1)
-            + word_count(Board, {Row - 1, Column - 1}, Node, Used2, Length + 1)
-            + word_count(Board, {Row - 1, Column + 1}, Node, Used2, Length + 1)
-            + word_count(Board, {Row + 1, Column - 1}, Node, Used2, Length + 1),
+          Count = word_count(Board, Row + 1, Column, Node, Used2, Length + 1)
+            + word_count(Board, Row - 1, Column, Node, Used2, Length + 1)
+            + word_count(Board, Row, Column + 1, Node, Used2, Length + 1)
+            + word_count(Board, Row, Column - 1, Node, Used2, Length + 1)
+            + word_count(Board, Row + 1, Column + 1, Node, Used2, Length + 1)
+            + word_count(Board, Row - 1, Column - 1, Node, Used2, Length + 1)
+            + word_count(Board, Row - 1, Column + 1, Node, Used2, Length + 1)
+            + word_count(Board, Row + 1, Column - 1, Node, Used2, Length + 1),
 
           case Terminal and (Length >= 3) of
             true -> 1 + Count;
